f7408195d7f1d33b7b92eeeaa5e6293cc00b73e7
jjdavenport <jordanjamesdavenport@gmail.com>
2025-03-02 21:37:21 +0000

    initial commit

---
+++ day_1/day1.py
@@ -0,0 +1,16 @@
+x = 1 + 2
+y = 1 - 2
+a = 1 & 4
+b = 4 / 8
+c = 4 ** 10
+d = 3023 // 124
+
+name = "john"
+surname = "smith"
+country  = "UK"
+enjoy = "I am enjoying 30 days of python"
+
+
+print(x, y, a, b, c, d, name, surname, country, enjoy, type(10), type(9.8), type(3.14), type(4 - 4j), type(['John', "Python", "Finland"]), type(name), type(country))
+
+print((2, 3), (4, 6))
